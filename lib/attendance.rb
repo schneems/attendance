24018fec66075a66bfdef8902519c48518d31b0a
Tobi Oduah <tobi.oduah@andela.com>
2018-09-01 20:09:02 +0100

    Alias blank? for ActiveRecord::Associations::CollectionProxy also

--- lib/attendance.rb
+++ lib/attendance.rb
@@ -6,11 +6,16 @@ end
 require 'active_record'
 
 ActiveRecord::Relation
+ActiveRecord::Associations::CollectionProxy
 
 module ActiveRecord
   # = Active Record Relation
   class Relation
     alias :blank? :empty?
+  end
 
+  # = Active Record Associations::CollectionProxy
+  class Associations::CollectionProxy
+    alias :blank? :empty?
   end
 end
